a860f741ac92bba04334869331052296634926d2
vremyavnikuda <andrew.nevsky@yandex.ru>
2024-09-16 20:07:55 +0700

    add doc
    add main interface

---
+++ doc.md
@@ -0,0 +1,12 @@
+## doc sl
+___
+
+[doc main](doc/main)
+
+[doc play_mp3](doc/play_mp3)
+
+[doc get_playlist](doc/get_playlist)
+
+[doc get_music_directory_from_config](doc/get_music_directory)
+
+[doc display_interface](doc/display_interface)
--- ui/display_interface.cpp
+++ ui/display_interface.cpp
@@ -2,8 +2,7 @@
 #include <vector>
 #include <string>
 
-void display_interface(const std::vector<std::string> &playlist, const int &track_index)
-{
+void display_interface(const std::vector<std::string> &playlist, const int &track_index) {
     int row, col;
     // Очищаем экран
     clear();
@@ -11,22 +10,38 @@ void display_interface(const std::vector<std::string> &playlist, const int &trac
     // Получаем размер окна терминала
     getmaxyx(stdscr, row, col);
 
+    //ширина рамки по горизонтали
+    int playlist_width = col - 2;
+    mvprintw(0, 0, "+");
+    mvprintw(0, playlist_width + 1, "+");
+
+    for (int i = 1; i < row - 1; ++i) {
+        mvprintw(i, 0, "|");
+        mvprintw(i, playlist_width + 1, "|");
+    }
+
+    mvprintw(row - 1, 0, "+");
+    mvprintw(row - 1, playlist_width + 1, "|");
+
+    for (int i = 1; i < playlist_width + 1; ++i) {
+        mvprintw(0, i, "-");
+        mvprintw(row - 1, i, "-");
+    }
+
     // Отображаем текущую песню в верхней строке
-    mvprintw(0, 0, "Now playing: %s", playlist[track_index].c_str());
+    mvprintw(0, 2, "Now playing: %s", playlist[track_index].c_str());
 
     // Отображаем список треков
-    for (int i = 0; i < playlist.size() && i < row - 1; ++i)
-    {
-        if (i == track_index)
-        {
+    for (int i = 0; i < playlist.size() && i < row - 3; ++i) {
+        if (i == track_index) {
             // Подсвечиваем текущий трек
             attron(A_REVERSE);
-            mvprintw(i + 1, 0, "%s", playlist[i].c_str());
+            // Сдвигаем на 2 символа вправо для рамки
+            mvprintw(i + 2, 2, "%s", playlist[i].c_str());
             attroff(A_REVERSE);
-        }
-        else
-        {
-            mvprintw(i + 1, 0, "%s", playlist[i].c_str());
+        } else {
+            // Сдвигаем на 2 символа вправо для рамки
+            mvprintw(i + 2, 2, "%s", playlist[i].c_str());
         }
     }
     // Обновляем экран
