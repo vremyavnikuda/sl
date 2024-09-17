542a7b7185c90245ae7bd6c8b4fd801d563d289d
vremyavnikuda <andrew.nevsky@yandex.ru>
2024-09-18 01:59:11 +0700

    fix ui bug
    - удалены не нужные файлы
    - переписаны исключения
    - fix bug с отображением  -> вместо названия песни указывался путь до песни
    - исправлен bug c переключением песни в плейлисте
    - интерфейс адаптивный  -> правильно отображается в терминале любого размера
    - добалены описания к методам
    - удалены не корректные комментарии к коду
    - удалена документация

--- service/play_mp3.cpp
+++ service/play_mp3.cpp
@@ -4,51 +4,73 @@
 #include <mpg123.h>
 #include <iostream>
 #include <thread>
+#include <ostream>
 
-void play_mp3(const std::string &file) {
-    ao_initialize();
-    mpg123_init();
-    int driver = ao_default_driver_id();
-    mpg123_handle *mh = mpg123_new(nullptr, nullptr);
+/* play_mp3
+Описание: Воспроизводит один MP3-файл, используя библиотеки ao для аудиовывода и mpg123 для декодирования MP3.
 
-    if (mpg123_open(mh, file.c_str()) != MPG123_OK) {
-        std::cerr << "Error opening MP3 file: " << file << std::endl;
-        return;
-    }
+Где используется: Используется в функции play_playlist для воспроизведения каждого трека по очереди.
 
-    long rate;
-    int channels, encoding;
-    mpg123_getformat(mh, &rate, &channels, &encoding);
+Влияние на код: Эта функция отвечает за воспроизведение одного трека. Она проверяет флаги playing, stop_playing, и exit_flag для управления процессом воспроизведения. Если stop_playing или exit_flag активны, воспроизведение прекращается.
 
-    ao_sample_format format;
-    format.bits = mpg123_encsize(encoding) * 8;
-    format.channels = channels;
-    format.rate = rate;
-    format.byte_format = AO_FMT_NATIVE;
-    format.matrix = nullptr;
+ТУТ ВООБЩЕ НИЧЕГО ТРОГАТЬ НЕЛЬЗЯ
+*/
+void play_mp3(const std::string &file)
+{
+    try
+    {
+        ao_initialize();
+        mpg123_init();
+        int driver = ao_default_driver_id();
+        mpg123_handle *mh = mpg123_new(nullptr, nullptr);
 
-    ao_device *device = ao_open_live(driver, &format, nullptr);
-    if (!device) {
-        std::cerr << "Error opening audio device." << std::endl;
-        return;
-    }
-
-    size_t done;
-    size_t buffer_size = mpg123_outblock(mh);
-    auto *buffer = new unsigned char[buffer_size];
-
-    while (mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK && !stop_playing && !exit_flag) {
-        if (playing) {
-            ao_play(device, reinterpret_cast<char *>(buffer), done);
-        } else {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        if (mpg123_open(mh, file.c_str()) != MPG123_OK)
+        {
+            throw std::runtime_error("Error opening MP3 file: " + file);
         }
-    }
 
-    delete[] buffer;
-    ao_close(device);
-    mpg123_close(mh);
-    mpg123_delete(mh);
-    mpg123_exit();
-    ao_shutdown();
+        long rate;
+        int channels, encoding;
+        mpg123_getformat(mh, &rate, &channels, &encoding);
+
+        ao_sample_format format;
+        format.bits = mpg123_encsize(encoding) * 8;
+        format.channels = channels;
+        format.rate = rate;
+        format.byte_format = AO_FMT_NATIVE;
+        format.matrix = nullptr;
+
+        ao_device *device = ao_open_live(driver, &format, nullptr);
+        if (!device)
+        {
+            throw std::runtime_error("Error opening audio device.");
+        }
+
+        size_t done;
+        size_t buffer_size = mpg123_outblock(mh);
+        auto *buffer = new unsigned char[buffer_size];
+
+        while (mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK && !stop_playing && !exit_flag)
+        {
+            if (playing)
+            {
+                ao_play(device, reinterpret_cast<char *>(buffer), done);
+            }
+            else
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            }
+        }
+
+        delete[] buffer;
+        ao_close(device);
+        mpg123_close(mh);
+        mpg123_delete(mh);
+        mpg123_exit();
+        ao_shutdown();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "An error occurred while playing the MP3 file: " << e.what() << std::endl;
+    }
 }
--- ui/display_interface.cpp
+++ ui/display_interface.cpp
@@ -1,49 +1,73 @@
 #include <ncurses.h>
 #include <vector>
 #include <string>
+#include <iostream>
 
-void display_interface(const std::vector<std::string> &playlist, const int &track_index) {
-    int row, col;
-    // Очищаем экран
-    clear();
+/*display_interface
 
-    // Получаем размер окна терминала
-    getmaxyx(stdscr, row, col);
+Описание: Отображает интерфейс на экране с помощью библиотеки ncurses, включая текущий трек, который воспроизводится, и список треков.
+-> add : форматирование отображения песни ,а не пути для файла (.mp3) в плейлисте
 
-    //ширина рамки по горизонтали
-    int playlist_width = col - 2;
-    mvprintw(0, 0, "+");
-    mvprintw(0, playlist_width + 1, "+");
+Где используется: Вызывается в основном цикле программы внутри функции main, чтобы обновить интерфейс на экране.
 
-    for (int i = 1; i < row - 1; ++i) {
-        mvprintw(i, 0, "|");
-        mvprintw(i, playlist_width + 1, "|");
-    }
+Влияние на код: Функция обновляет отображение текущего трека и позволяет пользователю видеть список воспроизводимых файлов. Обновление интерфейса происходит в цикле с частотой примерно каждые 100 миллисекунд, что поддерживает интерфейс интерактивным.
+*/
+void display_interface(const std::vector<std::string> &playlist, const int &track_index)
+{
+    try
+    {
+        int row, col;
+        // Очищаем экран
+        clear();
 
-    mvprintw(row - 1, 0, "+");
-    mvprintw(row - 1, playlist_width + 1, "|");
+        // Получаем размер окна терминала
+        getmaxyx(stdscr, row, col);
 
-    for (int i = 1; i < playlist_width + 1; ++i) {
-        mvprintw(0, i, "-");
-        mvprintw(row - 1, i, "-");
-    }
+        // ширина рамки по горизонтали
+        int playlist_width = col - 2;
+        mvprintw(0, 0, "+");
+        mvprintw(0, playlist_width + 1, "+");
 
-    // Отображаем текущую песню в верхней строке
-    mvprintw(0, 2, "Now playing: %s", playlist[track_index].c_str());
-
-    // Отображаем список треков
-    for (int i = 0; i < playlist.size() && i < row - 3; ++i) {
-        if (i == track_index) {
-            // Подсвечиваем текущий трек
-            attron(A_REVERSE);
-            // Сдвигаем на 2 символа вправо для рамки
-            mvprintw(i + 2, 2, "%s", playlist[i].c_str());
-            attroff(A_REVERSE);
-        } else {
-            // Сдвигаем на 2 символа вправо для рамки
-            mvprintw(i + 2, 2, "%s", playlist[i].c_str());
+        for (int i = 1; i < row - 1; ++i)
+        {
+            mvprintw(i, 0, "|");
+            mvprintw(i, playlist_width + 1, "|");
         }
+
+        mvprintw(row - 1, 0, "+");
+        mvprintw(row - 1, playlist_width + 1, "|");
+
+        for (int i = 1; i < playlist_width + 1; ++i)
+        {
+            mvprintw(0, i, "-");
+            mvprintw(row - 1, i, "-");
+        }
+
+        // Отображаем текущую песню в верхней строке
+        mvprintw(0, 2, "Now playing: %s", playlist[track_index].substr(playlist[track_index].find_last_of("/\\") + 1).c_str());
+
+        // Отображаем список треков
+        for (std::size_t i = 0; i < playlist.size() && i < row - 3; ++i)
+        {
+            if (i == track_index)
+            {
+                // Подсвечиваем текущий трек
+                attron(A_REVERSE);
+                // Сдвигаем на 2 символа вправо для рамки
+                mvprintw(i + 2, 2, "%s", playlist[i].substr(playlist[i].find_last_of("/\\") + 1).c_str());
+                attroff(A_REVERSE);
+            }
+            else
+            {
+                // Сдвигаем на 2 символа вправо для рамки
+                mvprintw(i + 2, 2, "%s", playlist[i].substr(playlist[i].find_last_of("/\\") + 1).c_str());
+            }
+        }
+        // Обновляем экрн
+        refresh();
     }
-    // Обновляем экран
-    refresh();
+    catch (const std::exception &e)
+    {
+        std::cerr << "An error occurred while displaying the interface: " << e.what() << std::endl;
+    }
 }
